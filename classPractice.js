da981e4759907e06b5a86a75fa0fcab2314a6ddc
ashtr0not21@gmail.com <ashtr0not21@gmail.com>
2025-03-25 11:30:07 -0700

    adding getter function

--- classPractice.js
+++ classPractice.js
@@ -1,17 +1,21 @@
 //Create a class and extend it
 
 class Vehicle{
-    constructor(wheels,doors,color,topSpeed){
+    constructor(wheels,doors,color,topSpeed, name){
         this.wheels = wheels 
         this.doors = doors
         this.color = color
         this.speed = topSpeed
+        this._name = name
     }
     vehicleOn(){
         console.log('Vehicle is turning on')
     }
     vehicleOff(){
         console.log('Vehicle is turning off')
+    }
+    get name(){ //This 
+        return this._name
     }
     
 }
@@ -23,4 +27,9 @@ class Motorcycle extends Vehicle{
     }
 }
 
-let suzuki = new Motorcycle(4,0,'black', 200, 'suzuki')
+let suzuki = new Motorcycle(4,0,'black', 200, 'suzuki', 'kalea')
+
+suzuki.name // kalea
+
+suzuki.name = 'other name' //this will not change the name to 'other name'
+
